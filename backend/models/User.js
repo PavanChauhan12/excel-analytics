7fb2dc537724e5240ac33332aa63891b1f2c2d91
Medapati Purna Nagendra Reddy <nagendrareddy0326@gmail.com>
2025-06-20 01:37:18 +0530

    fix: change email index from ascending to descending for unique constraint

--- backend/models/User.js
+++ backend/models/User.js
@@ -49,6 +49,6 @@ const UserSchema = new mongoose.Schema({
 });
 
 // Create index for unique email
-UserSchema.index({ email: 1 }, { unique: true });
+UserSchema.index({ email: -1 }, { unique: true });
 
 module.exports = mongoose.model('User', UserSchema);
